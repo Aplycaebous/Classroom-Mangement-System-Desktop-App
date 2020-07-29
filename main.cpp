4408492a2c069b0da286eaad297a7c6d24e5d731
Aplycaebous <45528856+Aplycaebous@users.noreply.github.com>
2020-07-29 13:31:17 +0600

    Revert "I changed the abstracter class and changed it to public"
    This reverts commit 9c70ec3da1264943b31118a0ac6b6c1458cf094b.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ private:
     static int no_of_buildings;
     static int student_id_size;
     static int staff_id_size;
-    //virtual void abstracter() = 0;//so that Metadata remains an abstract class
+    virtual void abstracter() = 0;//so that Metadata remains an abstract class
 protected:
     bool set_room_no_size(int val)
     {
@@ -84,7 +84,6 @@ public:
     {
         return staff_id_size;
     }
-    virtual void abstracter() = 0;
 };
 int Metadata::room_no_size = 3;
 int Metadata::no_of_buildings = 3;
@@ -433,7 +432,187 @@ public:
     }
 };
 
-
+class Admin:private Metadata
+{
+private:
+    string username;
+    string password;
+    string contact_no;
+    string email_address;
+    bool check_password(string val)
+    {
+        if(val.length()>32)
+        {
+            cout<<"Password for "<<get_username()<<" is too long"<<endl;
+            cout<<"Admin password must not contain more than 32 characters"<<endl;
+            return false;
+        }
+        else if(val.length()<8)
+        {
+            cout<<"Password for "<<get_username()<<" is too short"<<endl;
+            cout<<"Admin password must contain 8 characters at least"<<endl;
+            return false;
+        }
+        else
+        {
+            //check password complexity
+            //must contain at least one character and one digit or something like that
+            //admin password must be stronger than user password; make sure of that
+            return true;
+        }
+    }
+    bool check_contact_no_validity(string val)
+    {
+        if(val.size()==11)
+        {
+            for(int i=0;i<val.size();i++)
+            {
+                if(!isdigit(val[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+public:
+    Admin()
+    {
+        set_username("system");
+        set_password("system123");
+    }
+    Admin(string user_val, string pass_val, string cont_val,string email_val)
+    {
+        set_username(user_val);
+        set_password(pass_val);
+        set_contact_no(cont_val);
+        set_email_address(email_val);
+    }
+    bool set_username(string val)
+    {
+        if(val.length()>32)
+        {
+            cout<<val<<" is an invalid username"<<endl;
+            cout<<"Username must not contain more than 32 characters"<<endl;
+            return false;
+        }
+        else if(val.length()>=6)
+        {
+            username = val;
+            return true;
+        }
+        else
+        {
+            cout<<val<<" is an invalid username"<<endl;
+            cout<<"Username must contain 6 characters at least"<<endl;
+            return false;
+        }
+    }
+    bool set_password(string val)
+    {
+        if(check_password(val))
+        {
+            set_password(val);
+            return true;
+        }
+        return false;
+    }
+    bool set_contact_no(string val)
+    {
+        if(check_contact_no_validity(val))
+        {
+            contact_no = val;
+            return true;
+        }
+        else
+        {
+            cout << "Invalid contact number" << endl;
+            cout << "Make sure you DON'T write the country code before the number" << endl;
+            return false;
+        }
+    }
+    void set_email_address(string val)
+    {
+        email_address = val;
+    }
+    string get_username(void)
+    {
+        return username;
+    }
+    string get_password(void)
+    {
+        return password;
+    }
+    bool login(string user_val, string pass_val)
+    {
+        if(user_val == get_username() && pass_val == get_password())
+        {
+            return true;
+        }
+        else
+        {
+            cout<<"Wrong username/password"<<endl;
+            return false;
+        }
+    }
+    //In main function we create admin with username and password
+    //Admin can read and load classrooms (written)
+    //Admin can read and load user_list (written)
+    //Admin can check log file (written)
+    //Admin can edit metadata (Done)
+	
+	create_room_file(string path)
+	{
+		//the purpose of this function is to create our room file
+		//takes a file path as input and then reads data from that file (prefarably csv file)
+		//the file is the input for the room file stored in our system
+		//checks the validity of each entry of the file
+		//then each entry is appended in the room.txt file of our program
+		//preferable to append serially for easier access of other functions and readability
+		//there are multiple ways to take input serially
+		//firstly we can store all the validated data in the memory 
+		//then we write the whole array in the file
+	}
+	void add_room()//paramenter list for rooms 
+	{
+		//checks the room doesn't prexist. Physical_location is primary key, so check that
+		//for this we can pass a room[] array to this function
+		//then we search that array for that particular room
+		//finally we update the changed_array using create_room_file
+		
+		//adds a room in the room.txt file
+	}
+	create_student_file(string path)
+	{
+		//same thing we did for rooms but for student users
+		//creates student.txt file 
+	}
+	void add_student()
+	{
+		//checks the student doesn't prexist in the student.txt file
+		//appends a new student_user in the room.txt file
+	}
+	create_staff_file(string path)
+	{
+		//same thing we did for rooms but for staff users
+	}
+	void add_staff(){}
+	void add_admin(){}
+	//for the remove functions we pass an array of rooms. Then we remove from array of rooms 
+	//and then upload that array in the file 
+	remove_student()
+	remove_staff()
+	remove_admin()
+	remove_room()
+	void read_log(void)
+	{
+		//shows the log_file on the screen
+		//Present the data in a user friendly way (optional)
+	}
+	
+};
 
 class User
 {
@@ -502,37 +681,37 @@ public:
 		//
 		//Search the whole record array to see that the records don't overlap
 		//If it doesn't overlap... add the record and update the file(record.txt)
-		//If it overlaps show an error message
-		//to make it easier for you to work with these data I made some functions in date
+		//If it overlaps show an error message 
+		//to make it easier for you to work with these data I made some functions in date 
 		//and time for easier calculations and comparison
-		//call those functions when necessary
+		//call those functions when necessary 
 	}
 	void delete_record()
 	{
-		//sem thing, you understand, right?
+		//sem thing, you understand, right? 
 	}
 	void show_layout(void)
 	{
-		//Room and record are two seperated entities
+		//Room and record are two seperated entities 
 		//They have one to many relationship
-		//Physical_location is the foreign key in records
-		//There will be two seperate files for rooms and records
+		//Physical_location is the foreign key in records 
+		//There will be two seperate files for rooms and records 
 		//
 		//basically a display function which shows a list of all buildings (reads input from files)
 		//you can then select a particular building and it will show all the rooms in that building
 		//in our program, physical location is a combination of building number and room number
 		//hence, if you want you can show the list of building no + room number at a time, provided that the list isn't too long
-		//Remember to show them SERIALLY
+		//Remember to show them SERIALLY 
 		//you can then select a room and then it will show all the records in that room
 		//you should make at least 2 seperate functions for this, one to show physical_locations
 		//and another one to show all the records in a particular physical location
-		//the second function will be like this
-		//void display_records(Physical_location phy_loc_val)
+		//the second function will be like this 
+		//void display_records(Physical_location phy_loc_val)				
 	}
     //find classroom/search
-	//{
-	//	returns a physical_location after searching
-	//}
+	{
+		returns a physical_location after searching
+	}
 };
 
 class Student:public User
@@ -676,208 +855,6 @@ public:
         else return false;
     }
 };
-
-class Admin:public Metadata
-{
-private:
-    string username;
-    string password;
-    string contact_no;
-    string email_address;
-    bool check_password(string val)
-    {
-        if(val.length()>32)
-        {
-            cout<<"Password for "<<get_username()<<" is too long"<<endl;
-            cout<<"Admin password must not contain more than 32 characters"<<endl;
-            return false;
-        }
-        else if(val.length()<8)
-        {
-            cout<<"Password for "<<get_username()<<" is too short"<<endl;
-            cout<<"Admin password must contain 8 characters at least"<<endl;
-            return false;
-        }
-        else
-        {
-            //check password complexity
-            //must contain at least one character and one digit or something like that
-            //admin password must be stronger than user password; make sure of that
-            return true;
-        }
-    }
-    bool check_contact_no_validity(string val)
-    {
-        if(val.size()==11)
-        {
-            for(int i=0;i<val.size();i++)
-            {
-                if(!isdigit(val[i]))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        else
-            return false;
-    }
-public:
-    Admin()
-    {
-        set_username("system");
-        set_password("system123");
-    }
-    Admin(string user_val, string pass_val, string cont_val,string email_val)
-    {
-        set_username(user_val);
-        set_password(pass_val);
-        set_contact_no(cont_val);
-        set_email_address(email_val);
-    }
-    void set_username(string val)
-    {
-        if(val.length()>32)
-        {
-            cout<<val<<" is an invalid username"<<endl;
-            cout<<"Username must not contain more than 32 characters"<<endl;
-
-        }
-        else if(val.length()>=6)
-        {
-            username = val;
-            //return true;
-        }
-        else
-        {
-            cout<<val<<" is an invalid username"<<endl;
-            cout<<"Username must contain 6 characters at least"<<endl;
-           //return false;
-        }
-    }
-    void set_password(string val)
-    {
-        if(check_password(val))
-        {
-           // set_password(val);
-           password=val;
-           // return true;
-        }
-       // return false;
-    }
-    void set_contact_no(string val)
-    {
-        if(check_contact_no_validity(val))
-        {
-            contact_no = val;
-           // return true;
-        }
-        else
-        {
-            cout << "Invalid contact number" << endl;
-            cout << "Make sure you DON'T write the country code before the number" << endl;
-           // return false;
-        }
-    }
-    void set_email_address(string val)
-    {
-        email_address = val;
-    }
-    string get_username(void)
-    {
-        return username;
-    }
-    string get_password(void)
-    {
-        return password;
-    }
-    string get_email_address()
-    {
-        return email_address;//ADDED LATER
-    }
-    string get_contact_no()
-    {
-        return contact_no;//ADDED LATER
-    }
-    bool login(string user_val, string pass_val)
-    {
-        if(user_val == get_username() && pass_val == get_password())
-        {
-            return true;
-        }
-        else
-        {
-            cout<<"Wrong username/password"<<endl;
-            return false;
-        }
-    }
-    //In main function we create admin with username and password
-    //Admin can read and load classrooms (written)
-    //Admin can read and load user_list (written)
-    //Admin can check log file (written)
-    //Admin can edit metadata (Done)
-
-	create_room_file(string path)
-	{
-		//the purpose of this function is to create our room file
-		//takes a file path as input and then reads data from that file (prefarably csv file)
-		//the file is the input for the room file stored in our system
-		//checks the validity of each entry of the file
-		//then each entry is appended in the room.txt file of our program
-		//preferable to append serially for easier access of other functions and readability
-		//there are multiple ways to take input serially
-		//firstly we can store all the validated data in the memory
-		//then we write the whole array in the file
-	}
-	void add_room()//paramenter list for rooms
-	{
-		//checks the room doesn't prexist. Physical_location is primary key, so check that
-		//for this we can pass a room[] array to this function
-		//then we search that array for that particular room
-		//finally we update the changed_array using create_room_file
-
-		//adds a room in the room.txt file
-	}
-//	Student[] create_student_file(string path)
-	//{
-		//same thing we did for rooms but for student users
-		//creates student.txt file
-//	}
-	void add_student()
-	{
-		//checks the student doesn't prexist in the student.txt file
-		//appends a new student_user in the room.txt file
-	}
-//	Staff[] create_staff_file(string path)
-	//{
-		//same thing we did for rooms but for staff users
-//	}
-	void add_staff(){}
-	void add_admin(){}
-	//for the remove functions we pass an array of rooms. Then we remove from array of rooms
-	//and then upload that array in the file
-	remove_student();
-	remove_staff();
-	remove_admin();
-	remove_room();
-	void read_log(void)
-	{
-		//shows the log_file on the screen
-		//Present the data in a user friendly way (optional)
-	}
-	void abstracter()
-	{
-	    //ADDED LATER;
-
-	    cout<<get_username()<<endl;
-	    cout<<get_password()<<endl;
-	    cout<<get_email_address()<<endl;
-	    cout<<get_contact_no()<<endl;
-
-	}
-
-};
-
 
 class Record:public Physical_location, public Time, public Date
 {
@@ -1030,89 +1007,33 @@ public:
     }
 };
 
-class System_intializer
+class System_intializer()
 {
 	//Add a function which adds an empty Admin object if the Admin.txt file doesn't exist
-	//It can show a pop-up like first time use
+	//It can show a pop-up like first time use 
 	//New default admin created. Username: system. Password system123
 	//i.e. for first time use only
-public:
-
-	static Admin** read_admin()
+	
+	static Room[] read_room()
 	{
-	     Admin* f_list[100];
-	     fstream fin;
-	     int counti=0;
-
-    fin.open("Admin.csv", ios::in);
-    string line,word;;
-    string username,password,contact_no,email_address;
-
-    while (fin.good()) {
-           // cout<<"nana"<<endl;
-        getline(fin, line);
-        stringstream s(line);
-        int second_count=0;
-
-        while (getline(s, word, ',')) {
-            if(second_count==0)
-            {
-                   username=word;
-                second_count++;
-            }
-            else if(second_count==1)
-            {
-                    password=word;
-                second_count++;
-            }
-            else if(second_count==2)
-            {
-                    contact_no=word;
-                second_count++;
-            }
-            else if(second_count==3)
-            {
-                    email_address=word;
-                second_count++;
-            }
-
-
-        }
-        //cout<<username<<" "<<password<<" "<<contact_no<<" "<<email_address<<endl;
-        f_list[counti]=new Admin(username,password,contact_no,email_address) ;
-        //f_list[counti]->abstracter();
-        counti++;
-
-    }
-
-
-      
-        return f_list;
-
-
-
+		//reads the room array from room.txt and returns it
 	}
-
-	//static Student[] read_student();
-	static * read_staff();
-	static * read_record();
-};
+	static Student[] read_student()
+	static Staff[] read_staff()
+	static Admin[] read_admin()
+	static Record[] read_record()
+}
 
 //create a log class
-//{
+{
+	//log.txt file
 	//log class will contain all functions that will append the log file whenever anything is done in the program
 	//all the functions should be accessed by User and Admin classes
-//}
+}
 
 int main()
 {
-
-    Admin **ad;
-    ad=System_intializer::read_admin();
-
-
-
-
 	//all the files (room.txt, student.txt, staff.txt, admin.txt, records.txt
 	//will be read and stored in memory (class arrays) in the beginning of the function
+	
 }
