8132fbc43ec55e6abb2575c94a60e2ae2ed8bc36
Asir-Saadat <66594633+Asir-Saadat@users.noreply.github.com>
2020-09-15 02:10:43 +0600

    Main the classes to pointers

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    vector<Admin>ad;
+    vector<Admin*>ad;
     ad = System_initializer::read_admin();
 
-    vector<Room>r;
+    vector<Room*>r;
     r = System_initializer::read_room();
 
-    vector<Student>student;
+    vector<Student*>student;
     student = System_initializer::read_student();
 
-    vector<Staff>staff;
+    vector<Staff*>staff;
     staff = System_initializer::read_staff();
 
-    vector<Record>record;
+    vector<Record*>record;
     record = System_initializer::read_record();
     return a.exec();
 }
